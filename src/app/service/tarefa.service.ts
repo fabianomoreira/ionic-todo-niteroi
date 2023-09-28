c4e9592b03cbe9ed10b99c1facab0be6a7161478
Fabiano <professor@fabianomoreira.com.br>
2023-09-28 17:55:56 -0300

    Pequenos ajustes...

--- src/app/service/tarefa.service.ts
+++ src/app/service/tarefa.service.ts
@@ -1,14 +1,25 @@
 import { Observable } from "rxjs";
 import { Tarefa } from "../model/tarefa.model";
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpHeaders } from "@angular/common/http"
 import { TODO_API } from "../app.api";
 import { Injectable } from "@angular/core";
 
 @Injectable()
 export class TarefaService {
+    
+
     constructor(private http: HttpClient) {}
 
     listarTarefas(): Observable<Tarefa[]>{
-        return this.http.get<Tarefa[]>(`${TODO_API}/backtodo/rest/tarefa/listar`)
+        /*
+            const headers = new HttpHeaders({'Content-Type':'application/json', 
+                                             'Access-Control-Allow-Origin':'*',
+                                             'Access-Control-Allow-Methods':'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+                                             'Access-Control-Allow-Headers':'X-Requested-With,content-type',
+                                             'Access-Control-Allow-Credentials':'true'});
+  
+            const requestOptions = { headers: headers };
+        */
+        return this.http.get<Tarefa[]>(`${TODO_API}/backtodo/rest/tarefa/listar/`)
     }
 }
